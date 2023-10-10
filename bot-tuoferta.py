27485d30a1c4187d601d809bec60358c0c5cfa45
Hacksketch <114206828+MrJayrus@users.noreply.github.com>
2023-10-09 23:28:37 -0400

    Update bot-tuoferta.py

--- bot-tuoferta.py
+++ bot-tuoferta.py
@@ -1,6 +1,33 @@
 import telebot
 import os
-from bot-helpmsg import *
 
 # Inicializar el bot
 bot = telebot.TeleBot(os.environ['BOT_API'])
+
+# Manejar el comando /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    # Enviar mensaje de bienvenida
+    bot.reply_to(message, "¡Bienvenido! ¿Deseas comprar o vender algún producto?")
+
+    # Crear botones para elegir "VENDER" o "COMPRAR"
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = telebot.types.KeyboardButton('VENDER')
+    itembtn2 = telebot.types.KeyboardButton('COMPRAR')
+    markup.add(itembtn1, itembtn2)
+
+    # Enviar botones al usuario
+    bot.send_message(message.chat.id, "Elige una opción:", reply_markup=markup)
+
+# Manejar los mensajes del usuario después de que se hayan enviado los botones
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == "VENDER":
+        bot.reply_to(message, "Has elegido VENDER")
+    elif message.text == "COMPRAR":
+        bot.reply_to(message, "Has elegido COMPRAR")
+    else:
+        bot.reply_to(message, "No entiendo lo que quieres decir. Por favor, elige una opción.")
+
+# Iniciar el bot
+bot.polling()
